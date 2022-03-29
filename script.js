b3cdc327fef8c65d7e900bc905479cd4044f48a5
nick90903 <nhatchew03@gmail.com>
2022-03-29 06:17:54 -0500

    Added comparing logic

--- script.js
+++ script.js
@@ -34,5 +34,19 @@ let computerInput;
             }
         }
 
-
+        if(playerInput == computerInput) {
+            console.log('try again');
+        } else if (playerInput === 'rock' && computerInput === 'paper') {
+            console.log('Computer Wins');
+        } else if (playerInput === 'rock' && computerInput === 'scissors') {
+            console.log('Player Wins');
+        } else if (playerInput === 'paper' && computerInput === 'rock') {
+            console.log('Player Wins');
+        } else if (playerInput === 'paper' && computerInput === 'scissors') {
+            console.log('Computer Wins');
+        } else if (playerInput === 'scissors' && computerInput === 'rock') {
+            console.log('Computer Wins');
+        } else if (playerInput === 'scissors' && computerInput === 'paper') {
+            console.log('Player Wins');
+        }
     }
